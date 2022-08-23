f3946582c47a84344d297435d2f84829268727f3
z47985641 <z47985641@gmail.com>
2022-08-23 09:56:32 +0800

    加入專案檔案。

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            List<string> list = new List<string>() { "紅", "黃", "藍", "綠" };
+            Random rand = new Random();
+            int index = rand.Next(0, list.Count);
+            text1.Text = list[index];
+            list.RemoveAt(index);
+             index = rand.Next(0, list.Count);
+            text2.Text = list[index];
+            list.RemoveAt(index);
+             index = rand.Next(0, list.Count);
+            text3.Text = list[index];
+            list.RemoveAt(index);
+             index = rand.Next(0, list.Count);
+            text4.Text = list[index];
+            list.RemoveAt(index);
+
+        }
+
+    
+    }
+}
