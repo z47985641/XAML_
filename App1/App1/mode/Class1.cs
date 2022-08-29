58911d59cedeaca3f8738020b13da8143f1e3d9c
z47985641 <z47985641@gmail.com>
2022-08-29 09:50:29 +0800

    更新

--- App1/App1/mode/Class1.cs
+++ App1/App1/mode/Class1.cs
@@ -12,6 +12,10 @@ namespace App1.mode
         public string 理由 { get; set; }
         public string 日期 { get; set; }
         public bool 核可結果 { get; set; }
+        public override string ToString()
+        {
+            return this.日期 +"-"+ this.理由 +"["+this.申請人+"]";
+        }
 
     }
 }
